20f2fcf7669283e453d2c1d0f462c73e1357fa2e
bill.bo <junhu_bo@163.com>
2018-01-12 14:51:44 +0800

    JPsh :SDK3.0.7

---
+++ JiGuangDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JiGuangDemo
+//
+//  Created by bill.bo on 2018/1/12.
+//  Copyright © 2018年 bill.bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JiGuangDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JiGuangDemo
+//
+//  Created by bill.bo on 2018/1/12.
+//  Copyright © 2018年 bill.bo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
